67b3c78a7c9686e08e20c66c3712a9f1c269b934
Kristian de Lange <dubbelkris18@outlook.com>
2022-06-30 11:22:28 +0200

    Cleanup of comment

--- src/main/java/com/github/lunatrius/schematica/compat/ILOTRPresent.java
+++ src/main/java/com/github/lunatrius/schematica/compat/ILOTRPresent.java
@@ -4,7 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 
 /**
- * Interface for handling compatibility with other mods, based of this post by diesieben07: <a href="https://forums.minecraftforge.net/topic/63802-how-to-add-compatibility-with-other-mods/">...</a>
+ * Interface for handling compatibility with the lotr mod, based of this post by diesieben07: <a href="https://forums.minecraftforge.net/topic/63802-how-to-add-compatibility-with-other-mods/">...</a>
  */
 public interface ILOTRPresent {
     /**
@@ -12,15 +12,6 @@ public interface ILOTRPresent {
      * @return true if blacklisted for printer placement
      */
     Boolean isBlackListed(Block block, ItemStack itemStack);
-    /*
-        TODO:
-        fix rendering of tile entities (fix chat spam):
-    [16:15:45] [Client thread/ERROR]: ########## GL ERROR ##########
-    [16:15:45] [Client thread/ERROR]: @ Post render
-    [16:15:45] [Client thread/ERROR]: 1283: Stack overflow
-    [16:15:45] [Client thread/ERROR] [Schematica]: Failed to render a tile entity!
-    java.lang.NullPointerException
+    // TODO:fix printer functionality with weapon racks/ armour stands/ plates etc.
 
-        fix printer functionality with weapon racks/ armour stands/ plates etc.
-    */
 }

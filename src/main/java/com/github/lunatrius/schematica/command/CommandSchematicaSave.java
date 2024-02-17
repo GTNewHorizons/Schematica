a765d3fa4e917b0589d16f72c5ee64013cf207ca
Mist475 <dubbelkris18@outlook.com>
2024-02-17 13:40:53 +0100

    cleanup with jabel and generics

--- src/main/java/com/github/lunatrius/schematica/command/CommandSchematicaSave.java
+++ src/main/java/com/github/lunatrius/schematica/command/CommandSchematicaSave.java
@@ -31,11 +31,9 @@ public class CommandSchematicaSave extends CommandSchematicaBase {
             throw new WrongUsageException(getCommandUsage(sender));
         }
 
-        if (!(sender instanceof EntityPlayer)) {
+        if (!(sender instanceof EntityPlayer player)) {
             throw new CommandException(Names.Command.Save.Message.PLAYERS_ONLY);
         }
-
-        final EntityPlayer player = (EntityPlayer) sender;
 
         if (Schematica.proxy.isPlayerQuotaExceeded(player)) {
             throw new CommandException(Names.Command.Save.Message.QUOTA_EXCEEDED);

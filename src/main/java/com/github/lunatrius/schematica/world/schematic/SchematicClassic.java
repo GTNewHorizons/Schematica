359bdae77275f6406012e02e283a0b065ecffc7b
Mist475 <dubbelkris18@outlook.com>
2024-02-17 11:13:54 +0100

    migrate NotImplementedException to apache version

--- src/main/java/com/github/lunatrius/schematica/world/schematic/SchematicClassic.java
+++ src/main/java/com/github/lunatrius/schematica/world/schematic/SchematicClassic.java
@@ -2,20 +2,20 @@ package com.github.lunatrius.schematica.world.schematic;
 
 import net.minecraft.nbt.NBTTagCompound;
 
+import org.apache.commons.lang3.NotImplementedException;
+
 import com.github.lunatrius.schematica.api.ISchematic;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-// TODO: http://minecraft.gamepedia.com/Data_values_%28Classic%29
+// TODO: https://web.archive.org/web/20170708003352/http://minecraft.gamepedia.com/Data_values_(Classic)
 public class SchematicClassic extends SchematicFormat {
 
     @Override
     public ISchematic readFromNBT(NBTTagCompound tagCompound) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Not implemented");
     }
 
     @Override
     public boolean writeToNBT(NBTTagCompound tagCompound, ISchematic schematic) {
-        throw new NotImplementedException();
+        throw new NotImplementedException("Not implemented");
     }
 }
